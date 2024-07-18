b411c5c265a851b4d41246c376e43d5c63ea575b
srinath sriramula <sriramulasrinath59@gmail.com>
2024-07-18 11:54:42 +0530

    commiting changes

--- 10-Sg_grp/parametes.tf
+++ 10-Sg_grp/parametes.tf
@@ -38,6 +38,7 @@ resource "aws_ssm_parameter" "ingress_sg_id" {
   type  = "String"
   value = module.ingress.sg_id
 }
+
 resource "aws_ssm_parameter" "cluster_sg_id" {
   name  = "/${var.project_name}/${var.environment}/cluster_sg_id"
   type  = "String"
