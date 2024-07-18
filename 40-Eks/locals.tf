718774b444b348d579f295bb75bc32020628ac19
srinath sriramula <sriramulasrinath59@gmail.com>
2024-07-18 12:43:57 +0530

    commiting changes

--- 40-Eks/locals.tf
+++ 40-Eks/locals.tf
@@ -1,7 +1,6 @@
 locals {
   vpc_id = data.aws_ssm_parameter.vpc_id
   private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.id 
-  public_subnet_ids =  data.aws_ssm_parameter.public_subnet_ids.id 
   cluster_sg_id = data.aws_ssm_parameter.cluster_sg_id
   node_sg_id = data.aws_ssm_parameter.node_sg_id
 }
